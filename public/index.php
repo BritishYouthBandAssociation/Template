b76f130d9dfa80ff7a50160771bdfe0513302d5c
Luke Taylor <luke@circleleasing.co.uk>
2023-05-22 15:45:10 +0100

    Resolve templates

--- public/index.php
+++ public/index.php
@@ -4,16 +4,38 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+define("TEMPLATE_PATH", __DIR__ . "/../lib/Template/");
+
 require_once("../lib/Utils.php");
-require_once("../lib/Template/WordpressCoverTemplate.php");
 require_once("../lib/Config.php");
 
 $config = Config::load();
 
-$template = new WordpressCoverTemplate($config);
+$template = resolveTemplate($config);
 $template->parseParams();
 
 $image = $template->render();
 
 header("Content-Type: image/png");
 imagepng($image);
+
+function resolveTemplate($config){
+	$rawTemplate = $_GET['template'] . "Template.php";
+	$fileName = null;
+	$files = scandir(TEMPLATE_PATH);
+
+	foreach($files as $file){
+		if(strcasecmp($rawTemplate, $file) == 0){
+			$fileName = $file;
+		}
+	}
+
+	if($fileName == null){
+		die("Missing template");
+	}
+
+	$className = pathinfo($fileName, PATHINFO_FILENAME);
+
+	require_once(TEMPLATE_PATH . $fileName);
+	return new $className($config);
+}
