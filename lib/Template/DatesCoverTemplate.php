f6eeeb8d8bd4f220eb4734091c9b13bca856d6c9
Luke Taylor <luke@circleleasing.co.uk>
2023-06-02 10:26:43 +0100

    Fully customisable dates cover

--- lib/Template/DatesCoverTemplate.php
+++ lib/Template/DatesCoverTemplate.php
@@ -8,14 +8,23 @@ class DatesCoverTemplate extends BaseTemplate {
 	const HEIGHT = 1216;
 
 	protected $params = array(
-		"text" => ResourceTypes::STRING,
-		"shows" => [ResourceTypes::STRING, ResourceTypes::ARRAY]
+		"bannerText" => ResourceTypes::STRING,
+		"nextShow" => ResourceTypes::STRING,
+		"nextLocation" => ResourceTypes::STRING,
+		"nextDate" => ResourceTypes::STRING,
+		"shows" => [ResourceTypes::STRING, ResourceTypes::ARRAY],
+		"locations" => [ResourceTypes::STRING, ResourceTypes::ARRAY]
 	);
+
+	protected function validateParams()
+	{
+		return count($this->params["shows"]) == count($this->params["locations"]);
+	}
 
 	public function render(){
 		imagefill($this->canvas, 0, 0, $this->colours["primary"]);
-		alternatingTextStroke($this->canvas, 24, -20, 10, self::WIDTH, $this->fonts["title"], "BYBA 2023", $this->colours["light"], $this->colours["light"], 2);
-		alternatingTextStroke($this->canvas, 24, -350, self::HEIGHT - 34, self::WIDTH, $this->fonts["title"], "BYBA 2023", $this->colours["light"], $this->colours["light"], 2);
+		alternatingTextStroke($this->canvas, 24, -20, 10, self::WIDTH, $this->fonts["title"], $this->params["bannerText"], $this->colours["light"], $this->colours["light"], 2);
+		alternatingTextStroke($this->canvas, 24, -350, self::HEIGHT - 34, self::WIDTH, $this->fonts["title"], $this->params["bannerText"], $this->colours["light"], $this->colours["light"], 2);
 
 		$this->drawNextShow();
 		$this->drawDates();
@@ -33,28 +42,31 @@ class DatesCoverTemplate extends BaseTemplate {
 		writeCenteredTtfText($this->canvas, $this->fonts["title"], "NEXT SHOW", $this->colours["onPrimary"], $left, $top, $width, $headerSize);
 		$top += $padding + $headerSize;
 
-		writeCenteredTtfText($this->canvas, $this->fonts["title"], $this->params["text"], $this->colours["onPrimary"], $left, $top, $width, $dateSize);
+		writeCenteredTtfText($this->canvas, $this->fonts["title"], $this->params["nextDate"], $this->colours["onPrimary"], $left, $top, $width, $dateSize);
 		$top += $padding + $dateSize;
 
-		writeCenteredTtfText($this->canvas, $this->fonts["subtitle"], "Music Revolution", $this->colours["onPrimary"], $left, $top, $width, $headerSize);
+		writeCenteredTtfText($this->canvas, $this->fonts["subtitle"], $this->params["nextShow"], $this->colours["onPrimary"], $left, $top, $width, $headerSize);
 		$top += $padding + $headerSize;
 
-		writeCenteredTtfText($this->canvas, $this->fonts["subtitle"], "Belle Vue, Wakefield", $this->colours["onPrimary"], $left, $top, $width, $headerSize);
+		writeCenteredTtfText($this->canvas, $this->fonts["subtitle"], $this->params["nextLocation"], $this->colours["onPrimary"], $left, $top, $width, $headerSize);
 	}
 
 	private function drawDates(){
-		$top = self::HEIGHT / 4;
 		$left = self::WIDTH * 0.5;
 		$width = self::WIDTH / 3;
 		
 		$padding = 50;
 		$size = 30;
 		$subtitle = $size * 0.8;
+		$subtitlePadding = $padding / 2;
 
-		foreach($this->params["shows"] as $show){
-			writeCenteredTtfText($this->canvas, $this->fonts["title"], strtoupper($show), $this->colours["onPrimary"], $left, $top, $width, $size, $size);
-			$top += $size + ($padding / 2);
-			writeCenteredTtfText($this->canvas, $this->fonts["subtitle"], "Belle Vue, Wakefield - 18th June", $this->colours["onPrimary"], $left, $top, $width, $subtitle);
+		$estimatedSize = count($this->params["shows"]) * ($size + $padding + $subtitle + $subtitlePadding);
+		$top = (self::HEIGHT / 2) - ($estimatedSize / 2);
+
+		for($i = 0; $i < count($this->params["shows"]); $i++){
+			writeCenteredTtfText($this->canvas, $this->fonts["title"], strtoupper($this->params["shows"][$i]), $this->colours["onPrimary"], $left, $top, $width, $size, $size);
+			$top += $size + $subtitlePadding;
+			writeCenteredTtfText($this->canvas, $this->fonts["subtitle"], $this->params["locations"][$i], $this->colours["onPrimary"], $left, $top, $width, $subtitle);
 			$top += $size + $padding;
 		}
 	}
